8e6b1f408a1a33d823fa6970f8fad6f14a80ff9f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-07-03 21:19:01 -0300

    update

--- exercicio9.sql
+++ exercicio9.sql
@@ -18,7 +18,7 @@ SELECT * FROM cliente order by id_cliente ASC
 -- b. Complemento
 SELECT MAX(id_complemento) FROM complemento
 
-CREATE SEQUENCE complemento_id_seq MINVALUE 3
+CREATE SEQUENCE complemento_id_seq MINVALUE 3;
 
 ALTER TABLE complemento ALTER id_complemento SET DEFAULT nextval('complemento_id_seq');
 
@@ -28,9 +28,9 @@ ALTER SEQUENCE complemento_id_seq OWNED BY complemento.id_complemento
 
 SELECT MAX(id_fornecedor) FROM fornecedor
 
-CREATE SEQUENCE fornecedor_id_seq MINVALUE 4
+CREATE SEQUENCE fornecedor_id_seq MINVALUE 4;
 
-ALTER TABLE	fornecedor ALTER id_fornecedor SET DEFAULT nextval('fornecedor_id_seq')
+ALTER TABLE	fornecedor ALTER id_fornecedor SET DEFAULT nextval('fornecedor_id_seq');
 
 ALTER SEQUENCE fornecedor_id_seq OWNED BY fornecedor.id_fornecedor
 
@@ -38,16 +38,16 @@ ALTER SEQUENCE fornecedor_id_seq OWNED BY fornecedor.id_fornecedor
 
 SELECT MAX(id_municipio) FROM municipio
 
-CREATE SEQUENCE municipio_id_seq MINVALUE 11
+CREATE SEQUENCE municipio_id_seq MINVALUE 11;
 
-ALTER TABLE municipio ALTER id_municipio SET DEFAULT nextval('municipio_id_seq')
+ALTER TABLE municipio ALTER id_municipio SET DEFAULT nextval('municipio_id_seq');
 
 ALTER SEQUENCE municipio_id_seq OWNED BY municipio.id_municipio
 
 -- e. Nacionalidade
 SELECT MAX(id_nacionalidade) FROM nacionalidade
 
-CREATE SEQUENCE nacionalidade_id_seq MINVALUE 5
+CREATE SEQUENCE nacionalidade_id_seq MINVALUE 5;
 
 ALTER TABLE nacionalidade ALTER id_nacionalidade SET DEFAULT nextval('nacionalidade_id_seq');
 
@@ -57,7 +57,7 @@ ALTER SEQUENCE nacionalidade_id_seq OWNED BY nacionalidade.id_nacionalidade;
 
 SELECT MAX(id_pedido) FROM pedido
 
-CREATE SEQUENCE pedido_id_seq MINVALUE 16
+CREATE SEQUENCE pedido_id_seq MINVALUE 16;
 
 ALTER TABLE pedido ALTER id_pedido SET DEFAULT nextval('pedido_id_seq');
 
@@ -67,7 +67,7 @@ ALTER SEQUENCE pedido_id_seq OWNED BY pedido.id_pedido
 
 SELECT MAX(id_pedido) FROM pedido_produto
 
-CREATE SEQUENCE pdp_id_seq MINVALUE 16
+CREATE SEQUENCE pdp_id_seq MINVALUE 16;
 
 ALTER TABLE pedido_produto ALTER id_pedido SET DEFAULT nextval('pdp_id_seq');
 
@@ -76,7 +76,7 @@ ALTER SEQUENCE pdp_id_seq OWNED BY pedido_produto.id_pedido
 -- h. Profissão
 SELECT MAX(id_profissao) FROM profissao
 
-CREATE SEQUENCE profissao_id_seq MINVALUE 7
+CREATE SEQUENCE profissao_id_seq MINVALUE 7;
 
 ALTER TABLE profissao ALTER id_profissao SET DEFAULT nextval('profissao_id_seq');
 
@@ -85,7 +85,7 @@ ALTER SEQUENCE profissao_id_seq OWNED BY profissao.id_profissao
 -- i. Transportadora
 SELECT MAX(id_transportadora) FROM transportadora
 
-CREATE SEQUENCE transp_id_seq MINVALUE 3
+CREATE SEQUENCE transp_id_seq MINVALUE 3;
 
 ALTER TABLE transportadora ALTER id_transportadora SET DEFAULT nextval('transp_id_seq');
 
@@ -94,17 +94,17 @@ ALTER SEQUENCE transp_id_seq OWNED BY transportadora.id_transportadora
 -- j. UF
 SELECT MAX(id_uf) FROM uf
  
-CREATE SEQUENCE uf_id_seq MINVALUE 7
+CREATE SEQUENCE uf_id_seq MINVALUE 7;
 
-ALTER TABLE uf ALTER id_uf SET DEFAULT nextval('uf_id_seq')
+ALTER TABLE uf ALTER id_uf SET DEFAULT nextval('uf_id_seq');
 
 ALTER SEQUENCE uf_id_seq OWNED BY uf.id_uf
 
 -- k. Vendedor
 SELECT MAX(id_vendedor) FROM vendedor
 
-CREATE SEQUENCE vend_id_seq MINVALUE 9
+CREATE SEQUENCE vend_id_seq MINVALUE 9;
 
-ALTER TABLE vendedor ALTER id_vendedor SET DEFAULT nextval('vend_id_seq')
+ALTER TABLE vendedor ALTER id_vendedor SET DEFAULT nextval('vend_id_seq');
 
 ALTER SEQUENCE vend_id_seq OWNED BY vendedor.id_vendedor
