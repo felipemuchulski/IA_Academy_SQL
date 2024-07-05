a36cabfc00c5c4814bcb655c7fbae2af74175a80
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-07-04 21:19:13 -0300

    Exercicios de procedure

---
+++ exercicio13.sql
@@ -0,0 +1,7 @@
+-- Exercicios procedure
+-- Crie um stored procedure que receba como parâmetro o ID do produto e o percentual de aumento, e reajuste o preço somente deste produto de acordo com 
+-- o valor passado como parâmetro
+CREATE OR REPLACE PROCEDURE get_aumente_valor(idp INTEGER, percent FLOAT) LANGUAGE SQL AS 
+$$
+  
+$$;
